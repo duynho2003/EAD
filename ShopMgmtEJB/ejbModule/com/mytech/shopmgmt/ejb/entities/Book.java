f48bdf29edb53982ec6dc0d0caf949cf99311cb6
Tommy Nguyen <khenchaynguyen@gmail.com>
2024-03-11 19:21:04 +0700

    up

---
+++ ShopMgmtEJB/ejbModule/com/mytech/shopmgmt/ejb/entities/Book.java
@@ -0,0 +1,81 @@
+package com.mytech.shopmgmt.ejb.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "Books")
+public class Book implements Serializable{
+
+	private static final long serialVersionUID = -1316593709661341398L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(length = 256, unique = true)
+	@Size(min = 8, message = "Book name must at least 8 chars")
+	private String name;
+	
+	@Column(length = 1024)
+	private String preview;
+	
+	@Min(value = 0, message = "Book price must be min 0 VND")
+	private double price;
+	
+	@ManyToOne
+	@JoinColumn(name = "cat_id", nullable = false)
+	private Category category;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPreview() {
+		return preview;
+	}
+
+	public void setPreview(String preview) {
+		this.preview = preview;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	
+}
