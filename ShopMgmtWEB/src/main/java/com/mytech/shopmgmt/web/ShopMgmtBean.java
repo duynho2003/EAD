58dd8dc1966e3a038b1ca16bb40cea36b761d1b6
Tommy Nguyen <khenchaynguyen@gmail.com>
2024-03-13 16:41:14 +0700

    up xml javax to jakarta shopmgmt

--- ShopMgmtWEB/src/main/java/com/mytech/shopmgmt/web/ShopMgmtBean.java
+++ ShopMgmtWEB/src/main/java/com/mytech/shopmgmt/web/ShopMgmtBean.java
@@ -5,22 +5,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.mytech.shopmgmt.ejb.entities.Book;
+import com.mytech.shopmgmt.ejb.facades.BookFacadeRemote;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.EJB;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 
-@Named(value = "shopMgmtBean")
+@Named("shopMgmtBean")
 @SessionScoped
 public class ShopMgmtBean implements Serializable {
 
-	private static final long serialVersionUID = -686172580582109616L;
+	private static final long serialVersionUID = -4904541246430821656L;
+	
 	private List<Book> books;
-
+	
+	@EJB
+	private BookFacadeRemote bookFacade;
+	
 	public ShopMgmtBean() {
 		books = new ArrayList<Book>();
 	}
 
+	@PostConstruct
+	public void initialze() {
+		books = bookFacade.findAll();
+	}
+	
 	public List<Book> getBooks() {
 		return books;
 	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+	
+	
 }
