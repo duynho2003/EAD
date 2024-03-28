bf6a139571c2b2e570c56ba45a45eea832dd3f3e
Tommy Nguyen <khenchaynguyen@gmail.com>
2024-03-29 01:59:31 +0700

    up Pretest2 (add ok)

--- Pretest2WEB/src/main/java/com/mytech/pretest2/web/Pretest2Bean.java
+++ Pretest2WEB/src/main/java/com/mytech/pretest2/web/Pretest2Bean.java
@@ -14,25 +14,56 @@ import jakarta.inject.Named;
 
 @Named("pretest2Bean")
 @SessionScoped
-public class Pretest2Bean implements Serializable{
+public class Pretest2Bean implements Serializable {
 
 	private static final long serialVersionUID = -7223555914932449113L;
-	
+
+	// khai bao List model
 	private List<Laptop> laptops;
-	
+
 	private Laptop laptop;
-	
+
 	@EJB
 	private LaptopRemote laptopFacade;
-	
+
 	public Pretest2Bean() {
 		setLaptops(new ArrayList<Laptop>());
 	}
-	
+
 	@PostConstruct
 	public void initialze() {
 		setLaptops(laptopFacade.findAll());
 	}
+
+	// Add laptop (GET)
+	public String displayCreateLaptop() {
+		laptop = new Laptop();
+		return "add";
+	}
+	
+	// Edit laptop (GET)
+	public String displayUpdateLaptop(Laptop item) {
+		laptop = item;
+		return "edit";
+	}
+	
+	//Add laptop (POST)
+	public String performCreateLaptop() {
+		laptopFacade.create(laptop);
+		//
+		laptops = laptopFacade.findAll();
+		return "laptops";
+	}
+	
+	//xoa
+	public String deleteLaptop(Laptop item) {
+		laptopFacade.remove(item);
+		//
+		laptops = laptopFacade.findAll();
+		return "laptops";
+	}
+
+	// Getter & Setter
 
 	public List<Laptop> getLaptops() {
 		return laptops;
